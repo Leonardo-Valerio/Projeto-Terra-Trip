444f8923bae0d5194abfe05294df641be94e461d
Leonardo Moreira <leonardomoreiravalerio@gmail.com>
2024-01-14 23:51:23 -0300

    testei um for no meu dicionário que criei em views para exibir os continenstes que eu quero confrome a quantidade de continentes que adiciono no meu dicionário, proximo passo é criar meu banco de dados para isso

--- Documents/estudos/projetos pessoais/projeto-terra-trip/home/views.py
+++ Documents/estudos/projetos pessoais/projeto-terra-trip/home/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
+dados = {
+    1:{
+        'nome': 'Europa',
+        'img':''
+    },
+    2:{
+        'nome': 'América',
+        'img':''
+    }
+}
+
 def index(request):
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html', {'card':dados})
 
 def continente(request):
     return render(request, 'home/continente.html')
