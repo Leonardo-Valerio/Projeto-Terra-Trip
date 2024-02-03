33323e34f1b795ae58787651b9a11b075a6d8933
Leonardo Moreira <leonardomoreiravalerio@gmail.com>
2024-02-03 00:56:56 -0300

    adicionei todas as cidades e pontos turisticos e paises da europa no meu banco de dados

--- Documents/estudos/projetos pessoais/projeto-terra-trip/home/urls.py
+++ Documents/estudos/projetos pessoais/projeto-terra-trip/home/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', index, name='home'),
     path('continente/<int:img_id>',continente, name='continente'),
     path('pais/<int:pais_id>',pais,name='pais'),
-    path('/sobre_nos',sobre_nos, name='sobre_nos')
+    path('sobre_nos',sobre_nos, name='sobre_nos')
 ]
