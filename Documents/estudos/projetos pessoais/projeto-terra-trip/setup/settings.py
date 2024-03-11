f508cf0702af73c40a2375e72a376203fa2f8e1e
Leonardo Moreira <leonardomoreiravalerio@gmail.com>
2024-03-11 13:26:09 -0300

    exclui arquivos heroku

--- Documents/estudos/projetos pessoais/projeto-terra-trip/setup/settings.py
+++ Documents/estudos/projetos pessoais/projeto-terra-trip/setup/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
